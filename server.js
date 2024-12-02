ae85e4b2c8cdaf05376ba5beba5e871c0d565140
Alfredopc22 <115669120+Alfredopc22@users.noreply.github.com>
2024-12-01 22:26:02 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+// Importar módulos:
+const express = require('express');
+const path = require('path');
+const router = require('./App/controllers/router'); 
+const cors = require('cors');
+
+const app = express();
+
+// Levantar el servidor en el puerto 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Aplicación de ejemplo corriendo en el puerto ${port}`);
+});
+
+// Middleware para habilitar CORS
+app.use(cors());
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Middleware para usar el router principal
+app.use('/', router); // Usa el router que ya tiene las rutas definidas
+
+// Middleware para servir archivos estáticos
+app.use(express.static('App'));
+app.use('/Views', express.static('Views'));
+
+
+// imprime el mensaje cuando se corre el servidor
+app.listen(port, () => {
+    console.log("Aplicación de ejemplo corriendo en el puerto " + port);
+});
